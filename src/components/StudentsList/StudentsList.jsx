c41a3556242e6f7986790d80760c476f70b1abd9
Tornike512 <Dht5152@gmail.com>
2023-12-02 23:49:36 +0400

    changed back to where this code was before last commit

--- src/components/StudentsList/StudentsList.jsx
+++ src/components/StudentsList/StudentsList.jsx
@@ -1,10 +1,20 @@
 import { useContext, useState } from "react";
 import { AppContext } from "../../AppContext";
-import { v4 as uuidv4 } from "uuid";
 import "./StudentsList.scss";
 
 export function StudentsList() {
+  const { students, input } = useContext(AppContext);
+
   const [edit, setEdit] = useState(false);
+  const [value, setValue] = useState("-");
+
+  function changeText() {
+    setEdit(!edit);
+  }
+
+  function buttonValue() {
+    setEdit(false);
+  }
 
   return (
     <>
@@ -21,6 +31,72 @@ export function StudentsList() {
           <h1 className="friday">პარასკევი</h1>
         </div>
       </div>
+
+      {students.map((student) => (
+        <div className="new-student-spacing" key={student.id}>
+          <div className="student-spacing">
+            <h1 className="new-student">{student.firstName}</h1>
+            <h1 className="new-student">{student.lastName}</h1>
+            <div className="point-spacing">
+              {edit ? (
+                <input
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  onBlur={buttonValue}
+                  type="text"
+                  placeholder="-"
+                />
+              ) : (
+                <button onClick={changeText}>{value}</button>
+              )}
+              {edit ? (
+                <input
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  onBlur={buttonValue}
+                  type="text"
+                  placeholder="-"
+                />
+              ) : (
+                <button onClick={changeText}>{value}</button>
+              )}
+              {edit ? (
+                <input
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  onBlur={buttonValue}
+                  type="text"
+                  placeholder="-"
+                />
+              ) : (
+                <button onClick={changeText}>{value}</button>
+              )}
+              {edit ? (
+                <input
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  onBlur={buttonValue}
+                  type="text"
+                  placeholder="-"
+                />
+              ) : (
+                <button onClick={changeText}>{value}</button>
+              )}
+              {edit ? (
+                <input
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  onBlur={buttonValue}
+                  type="text"
+                  placeholder="-"
+                />
+              ) : (
+                <button onClick={changeText}>{value}</button>
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
