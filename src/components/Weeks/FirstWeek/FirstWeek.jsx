da12c2252172a3c622ece884d4336adc07b76a6c
Tornike512 <Dht5152@gmail.com>
2023-12-03 19:07:27 +0400

    added same functonality to firstWeek button

--- src/components/Weeks/FirstWeek/FirstWeek.jsx
+++ src/components/Weeks/FirstWeek/FirstWeek.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { AppContext } from "../../../AppContext";
 import "./FirstWeek.scss";
 
@@ -7,21 +7,25 @@ export function FirstWeek() {
     useContext(AppContext);
 
   const firstWeek = weeks[0];
+  const weekButtonClicked = useRef();
+
+  useEffect(() => {
+    weekButtonClicked.current.click();
+  }, []);
 
   return (
-    <>
-      <button
-        onClick={() => {
-          setCurrentWeek(firstWeek.id);
-          setSelectedWeek("firstWeek");
-        }}
-        className={
-          selectedWeek === "firstWeek" ? "toggled-first-week" : "first-week"
-        }
-        key={firstWeek.id}
-      >
-        {firstWeek.name}
-      </button>
-    </>
+    <button
+      ref={weekButtonClicked}
+      onClick={() => {
+        setCurrentWeek(firstWeek.id);
+        setSelectedWeek("firstWeek");
+      }}
+      className={
+        selectedWeek === "firstWeek" ? "toggled-first-week" : "first-week"
+      }
+      key={firstWeek.id}
+    >
+      {firstWeek.name}
+    </button>
   );
 }
